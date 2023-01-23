0311c8c0d510c09a495fa2a5134d2f3bcf9762e4
Brayan Santos <brayan.santosrosa@gmail.com>
2023-01-23 18:08:58 -0300

    Registro feito com sucesso

--- back-end/src/api/services/loginService.js
+++ back-end/src/api/services/loginService.js
@@ -1,10 +1,10 @@
-const { createHash } = require('node:crypto');
 const { User } = require('../../database/models');
 const { generateToken } = require('../../utils/jwt');
+const hash = require('../../utils/hash');
 
 const login = async (obj) => {
     const throwError = { status: 404, message: 'Not Found' };
-    const senha = createHash('md5').update(obj.password).digest('hex');
+    const senha = hash(obj.password);
     const response = await User.findOne({ where: { email: obj.email, password: senha } });
     if (!response) throw throwError;
     const { password, ...newObj } = response.dataValues;
