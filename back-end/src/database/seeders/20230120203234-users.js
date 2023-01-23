3a2b01ad2043dd6c42818ca2d9b065c2be67e848
Henryck <ryck302@gmail.com>
2023-01-23 00:39:33 -0300

    consertar seed (agora os testes estão rodando)

--- back-end/src/database/seeders/20230120203234-users.js
+++ back-end/src/database/seeders/20230120203234-users.js
@@ -11,7 +11,7 @@ module.exports = {
    }, 
   {
     id: 2 ,
-    name: 'Fulana Perreira',
+    name: 'Fulana Pereira',
     email: 'fulana@deliveryapp.com' ,
     password: '3c28d2b0881bf46457a853e0b07531c6',
     role: 'seller' ,
