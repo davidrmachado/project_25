6a39eadc439b11d7c3e0620d2c10c8ee29957d14
Henryck <ryck302@gmail.com>
2023-01-24 20:38:03 -0300

    fix requisito 11

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       </Route>
       <Route path="/login" component={ Login } />
       <Route path="/register" component={ Register } />
-      <Route path="/products" component={ Products } />
+      <Route path="/customer/products" component={ Products } />
     </BrowserRouter>
   );
 }
