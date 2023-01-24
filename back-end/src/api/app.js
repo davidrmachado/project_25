65557fc1433b14aee3e3f6e27b12c3d35a64c48d
Brayan Santos <brayan.santosrosa@gmail.com>
2023-01-24 15:16:36 -0300

    Refatorando

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cards = require('./Routes/cardsRouter');
  require('express-async-errors');
 const login = require('./Routes/LoginRouter');
 const register = require('./Routes/registerRouter');
+const error = require('../utils/errorHandle');
 
 const app = express();
 
@@ -9,7 +11,12 @@ app.use(express.json());
 
 app.use('/login', login);
 app.use('/register', register);
+app.use('/customer', cards);
 
-app.use((err, req, res, _next) => res.status(err.status).json({ message: err.message }));
+app.use((err, req, res, _next) => { 
+    if (err.message === 'Not Found') {
+    return res.status(err.status).json(error[404]);
+    }
+});
 
 module.exports = app;
---
+++ back-end/src/api/controller/cardsController.js
@@ -0,0 +1,8 @@
+const { allCards } = require('../services/cardsService');
+
+const cardsController = async (_req, res) => {
+const result = await allCards();
+return res.status(200).json(result);
+};
+
+module.exports = { cardsController };
