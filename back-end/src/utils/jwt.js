6d396d047ba5ca6748812949c086dbeaedd19791
Henryck <ryck302@gmail.com>
2023-01-25 18:43:10 -0300

    fix jwt

--- back-end/src/utils/jwt.js
+++ back-end/src/utils/jwt.js
@@ -1,9 +1,22 @@
 require('dotenv').config();
+/* const fs = require('fs');  */
 const { sign, verify } = require('jsonwebtoken');
+/* const path = require('path');
+ */
+/* const keyPath = path.join(__dirname, '../jwt.evaluation.key');
+
+const JWT_KEY = () => {
+  try {
+    const jwt = fs.readFileSync(keyPath, 'utf8');
+    return jwt;
+  } catch (err) {
+    console.error(err);
+  }
+}; */
 
 const generateToken = (obj) => {
     const { password, ...newObj } = obj;
-    const token = sign({ ...newObj }, process.env.JWT_SECRET, {
+    const token = sign({ ...newObj }, 'secret_key', {
         expiresIn: '1d',
         algorithm: 'HS256',
     });
@@ -13,7 +26,7 @@ const generateToken = (obj) => {
 
 const verifyToken = (token) => {
     try {
-        const data = verify(token, process.env.JWT_SECRET);
+        const data = verify(token, 'secret_key');
         return data;
     } catch (err) {
         const throwError = { status: 401, message: 'Expired or invalid token' };
