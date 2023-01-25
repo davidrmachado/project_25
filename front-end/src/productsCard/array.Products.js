655e6b8eec6a2562d67b52a138777c22be35e256
eduardo <eduferreiracos@gmail.com>
2023-01-24 22:06:19 -0300

    add req.12-tela de produtos

---
+++ front-end/src/productsCard/array.Products.js
@@ -0,0 +1,67 @@
+import prod1 from './imagens/skol_lata_350ml.jpg'
+import prod2 from './images/heineken_600ml.jpg';
+import prod3 from './images/antarctica_pilsen_300ml.jpg';
+import prod4 from './images/brahma_600ml.jpg';
+import prod5 from './images/skol_269ml.jpg';
+import prod6 from './images/skol_beats_senses_313ml.jpg';
+import prod7 from './images/becks_330ml.jpg';
+import prod8 from './images/brahma_duplo_malte_350ml.jpg';
+import prod9 from './images/becks_600ml.jpg';
+import prod10 from './images/skol_beats_senses_269ml.jpg';
+import prod11 from './images/stella_artois_275ml.jpg';
+
+const arrayProducts = [
+  { 
+    id: 1,
+    name: 'Skol Lata 250ml',
+    price: 2.20, url_image: prod1,
+  },
+  { id: 2,
+    name: 'Heineken 600ml',
+    price: 7.50,
+    url_image: prod2,
+  },
+  { id: 3,
+    name: 'Antarctica Pilsen 300ml',
+    price: 2.49, url_image: prod3,
+  },
+  { id: 4,
+    name: 'Brahma 600ml',
+    price: 7.50,
+    url_image: prod4,
+  },
+  { id: 5,
+    name: 'Skol 269ml',
+    price: 2.19,
+    url_image: prod5,
+  },
+  { id: 6,
+    name: 'Skol Beats Senses 313ml',
+    price: 4.49,
+    url_image: prod6,
+  },
+  { id: 7,
+    name: 'Becks 330ml',
+    price: 4.99,
+    url_image: prod7,
+  },
+  { id: 8,
+    name: 'Brahma Duplo Malte 350ml',
+    price: 2.79, url_image: prod8,
+  },
+  { id: 9,
+    name: 'Becks 600ml',
+    price: 8.89,
+    url_image: prod9,
+  },
+  { id: 10,
+    name: 'Skol Beats Senses 269ml',
+    price: 3.57,
+    url_image: prod10 },
+  { id: 11,
+    name: 'Stella Artois 275ml',
+    price: 3.49,
+    url_image: prod11 },
+  ]
+
+export default arrayProducts;
