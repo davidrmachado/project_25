c31d59de05c52d1669000877e14a83bfa3f2549b
Eduardo Santos <eduardo.ocara@gmail.com>
2023-02-01 09:49:57 -0300

    criado o CSS da tela de Produtos

--- front-end/src/components/ItemCards.jsx
+++ front-end/src/components/ItemCards.jsx
@@ -76,50 +76,63 @@ function ItemCard() {
   };
 
   return (
-    <div>
+    <div className="products_list">
       { products.map((product) => (
-        <div
+        <article
           key={ product.id }
           data-testid={ `customer_products__element-card-price-${product.id}` }
         >
-          <p
-            data-testid={ `customer_products__element-card-price-${product.id}` }
-          >
-            { product.price.toString().replace('.', ',') }
-          </p>
-          <img
-            data-testid={ `customer_products__img-card-bg-image-${product.id}` }
-            src={ product.url_image }
-            alt="product demo"
-            className="drink-image"
-          />
-          <h1
-            data-testid={ `customer_products__element-card-title-${product.id}` }
-          >
-            { product.name }
-          </h1>
-          <button
-            type="button"
-            data-testid={ `customer_products__button-card-rm-item-${product.id}` }
-            onClick={ () => handleRemoveButton(product.id) }
-          >
-            -
-          </button>
-          <input
-            data-testid={ `customer_products__input-card-quantity-${product.id}` }
-            type="number"
-            value={ product.quantity }
-            onChange={ (e) => changeQuantity(e, product.id) }
-            min="0"
-          />
-          <button
-            type="button"
-            data-testid={ `customer_products__button-card-add-item-${product.id}` }
-            onClick={ () => handleAddButton(product.id) }
-          >
-            +
-          </button>
-        </div>
+          <div className="title">
+            <h2
+              data-testid={ `customer_products__element-card-title-${product.id}` }
+            >
+              { product.name }
+            </h2>
+          </div>
+          <div className="img_drink_container">
+            <img
+              data-testid={ `customer_products__img-card-bg-image-${product.id}` }
+              src={ product.url_image }
+              alt="product demo"
+              // className="drink-image"
+            />
+          </div>
+          <div className="item_price_container">
+            <h4
+              data-testid={ `customer_products__element-card-price-${product.id}` }
+            >
+              R$
+              {' '}
+              { product.price.toString().replace('.', ',') }
+            </h4>
+          </div>
+          <div className="select_quant_container">
+            <button
+              type="button"
+              className="add_delete_quantity"
+              data-testid={ `customer_products__button-card-rm-item-${product.id}` }
+              onClick={ () => handleRemoveButton(product.id) }
+            >
+              -
+            </button>
+            <input
+              data-testid={ `customer_products__input-card-quantity-${product.id}` }
+              type="number"
+              className="quantity_input"
+              value={ product.quantity }
+              onChange={ (e) => changeQuantity(e, product.id) }
+              min="0"
+            />
+            <button
+              type="button"
+              className="add_delete_quantity"
+              data-testid={ `customer_products__button-card-add-item-${product.id}` }
+              onClick={ () => handleAddButton(product.id) }
+            >
+              +
+            </button>
+          </div>
+        </article>
       )) }
     </div>
   );
--- front-end/src/components/Total.jsx
+++ front-end/src/components/Total.jsx
@@ -10,14 +10,20 @@ function Total() {
     .reduce((acc, cur) => acc + cur, 0).toFixed(2).replace('.', ',');
 
   return (
-    <div>
+    <div className="customer_button-cart">
       <button
         type="button"
+        className="button_cart"
         data-testid="customer_products__button-cart"
         onClick={ () => navigate.push('/customer/checkout') }
         disabled={ !cart.length > 0 }
       >
-        Meu Carrinho: R$
+        <p id="total">
+          Total:
+          {' '}
+          R$
+          {' '}
+        </p>
         <p data-testid="customer_products__checkout-bottom-value">
           {`${total || 0}`}
         </p>
