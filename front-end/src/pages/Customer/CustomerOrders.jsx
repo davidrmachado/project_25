96a7201ee484383d41fe7790e66e9e4c4eba1dad
Eduardo Santos <eduardo.ocara@gmail.com>
2023-02-02 17:11:07 -0300

    criado o CSS da tela de Orders | Order Details

--- front-end/src/pages/Customer/CustomerOrders.jsx
+++ front-end/src/pages/Customer/CustomerOrders.jsx
@@ -20,34 +20,47 @@ function Orders() {
   return (
     <>
       <Navbar />
-      <div>
+      <div className="orders_container">
         { orders ? (
           orders.map((order) => (
-            <Link key={ order.id } to={ `/customer/orders/${order.id}` }>
-              <p
-                data-testid={ `customer_orders__element-order-id-${order.id}` }
-              >
-                {String(order.id).padStart(orderNumberLength, '0')}
-              </p>
+            <div key={ order.id } className="order_card">
+              <div className="number_order">
+                <Link to={ `/customer/orders/${order.id}` }>
+                  <p
+                    className="order_title"
+                    data-testid={ `customer_orders__element-order-id-${order.id}` }
+                  >
+                    Pedido nº
+                    { ' ' }
+                    {String(order.id).padStart(orderNumberLength, '0')}
+                  </p>
+                </Link>
+              </div>
 
               <p
                 data-testid={ `customer_orders__element-delivery-status-${order.id}` }
               >
+                <b>Status:</b>
+                {' '}
                 {order.status}
               </p>
 
               <p
                 data-testid={ `customer_orders__element-order-date-${order.id}` }
               >
+                <b>Data:</b>
+                {' '}
                 { moment(`${order.saleDate}`).format('DD/MM/YYYY') }
               </p>
 
               <p
                 data-testid={ `customer_orders__element-card-price-${order.id}` }
               >
+                <b>Valor Total:</b>
+                {' '}
                 {`R$ ${order.totalPrice.toString().replace('.', ',')}` }
               </p>
-            </Link>
+            </div>
           ))
         ) : <p> Você não tem pedidos </p> }
       </div>
